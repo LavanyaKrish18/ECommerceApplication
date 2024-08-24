ac7be95a5de2a3df2c845f2c989eb42c0ee9ff6b
LavanyaKrishnan <lavanyakrishnan18071998@gmail.com>
2024-08-24 21:17:53 +0530

    [ProductCatalogService] Implementation for FetchMode, FetchTypes understanding

--- src/main/java/org/example/productcatalogservice/models/Category.java
+++ src/main/java/org/example/productcatalogservice/models/Category.java
@@ -5,6 +5,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -18,6 +21,14 @@ public class Category extends BaseModel{
     private String description;
 
     // Fetch type by default is LAZY
-    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY) //LAZY, EAGER
+    @Fetch(FetchMode.SELECT) // SELECT, JOIN, SUBSELECT
+    @BatchSize(size = 3) // 1, 6, 3
     private List<Product> products;
 }
+
+// BatchSize
+//N+1 Problem  => 1 + 6 ,
+//if(n == N) -> 1 + 1 n=6 -> 1 + 1
+//if(n < N) -> 1 + N/n , n = 3 ,  1+2
+//if(n>N) -> 1+1
--- src/test/java/org/example/productcatalogservice/repos/CategoryRepoTest.java
+++ src/test/java/org/example/productcatalogservice/repos/CategoryRepoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,23 +20,46 @@ class CategoryRepoTest {
 
     @Test // whatever saved or updated in DB will be rolled back as it is a test class
     @Transactional // execute all or nothing
-    public void demonstateLoading(){
+    public void demonstrateLoading() {
         Optional<Category> optionalCategory = categoryRepo.findById(2L);
 
-        if(optionalCategory.isPresent()){
+        if (optionalCategory.isPresent()) {
             Category c = optionalCategory.get();
 //            for (Product product : c.getProducts()){
 //                System.out.println(product.getName() + product.getCategory().getName());
 //            }
         }
-
-        //Fetch type of Category class - products field is Lazy
-            // If asked for products 2 queries - fetch category and products tables
-            // If not asked for products 1 query - fetch category table
-        //Fetch type of Category class - products field is Eager - Run 1 left join query
-            // If asked for products 2 queries - fetch category and products tables
-            // If not asked for products 2 queries - fetch category and products tables
-
     }
 
+        @Test
+        @Transactional
+        public void demonstrateFetchModeFetchType() {
+        //used to fill FetchMode-FetchType-Results table
+            Optional<Category> optionalCategory = categoryRepo.findById(2L);
+
+            if (optionalCategory.isPresent()) {
+                Category c = optionalCategory.get();
+                for (Product product : c.getProducts()) {
+                    System.out.println(product.getName() + product.getCategory().getName());
+                }
+            }
+        }
+
+    @Test
+    @Transactional
+    // One of the solutions to nPlusOneProblem is SubSelect
+    // 2nd solution - use SELECT with Batch size
+    // JOIN gives same result as SELECT which causes nPlusOneProblem
+    // As per chatGPT, join can also reduce to 1 query. but not working in our example
+    public void nPlusOneProblem () { // demonstrateFetchModeSubSelect
+        // Try first LAZY, SELECT - 1 query to fetch all category and
+            // N query to getProducts for each category. N = category. nPlusOneProblem
+        //Try LAZY, SUBSELECT
+        List<Category> categoryList = categoryRepo.findAll();
+        for(Category category : categoryList) {
+            for (Product product : category.getProducts()) {
+                System.out.println(product.getName() + product.getCategory().getName());
+            }
+        }
+    }
 }
